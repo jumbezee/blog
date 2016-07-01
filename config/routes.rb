ceedf81924122adb3a4a1c33de9b3d82c0cb112f
Nikolay <kuz-kol@mail.ru>
2016-07-02 00:37:34 +0300

    add one-to-many article comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
   
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
