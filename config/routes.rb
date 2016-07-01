a86b6105288c268f2063b2d57d108f4b0518ab5d
Nikolay <kuz-kol@mail.ru>
2016-07-01 23:42:34 +0300

    Add delete button

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
+  
   resources :articles
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
 
