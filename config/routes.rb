94a1f8bb81404b2d491c40e946c4353a0aaaf02c
Nikolay <kuz-kol@mail.ru>
2016-07-01 19:04:44 +0300

    add contacts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :articles
-  resources :contacts
+  resource :contacts, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
