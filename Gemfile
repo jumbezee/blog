95796c5a6f2378a993f3d18e1908f72f999a390d
Nikolay <kuz-kol@mail.ru>
2016-07-02 23:58:46 +0300

    install rspec for rails

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
-source 'https://rubygems.org'
+ source 'https://rubygems.org'
 
+group :test, :development do
+  gem "rspec-rails", "~> 2.4"
+  gem 'capybara'
+end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
   def create
     @article = Article.find(params[:article_id])
+    
     @article.comments.create(comment_params)
 
     redirect_to article_path(@article)
@@ -10,7 +11,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body)
   end
 
 end
