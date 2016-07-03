7bf0dc4d57613b700c407a782e44993a6fd3304f
Nikolay <kuz-kol@mail.ru>
2016-07-03 03:09:57 +0300

    Update acceptence test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 feature "Contact Creation" do
-  scenario "allows acees to contacts page" do
+  scenario "allows acces to contacts page" do
     visit '/contacts'
 
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content (I18n.t 'contacts.contact_us')
+  end
+
+  scenario "allows a quest to create contact" do
+    visit '/contacts'
+
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+
+    click_button 'Send Message'
+
+    expect(page).to have_content 'Thanks!!!'
   end
 end
